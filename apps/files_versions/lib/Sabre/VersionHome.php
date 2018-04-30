8991ff9ddccb38d7e1bd5db0aabe88357a4323a2
Roeland Jago Douma <roeland@famdouma.nl>
2018-04-30 20:39:45 +0200

    Add version collection to DAV
    listed as
    versions/<username>/versions/<file id>/<timastamp>
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

---
+++ apps/files_versions/lib/Sabre/VersionHome.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * @copyright 2018, Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * @author Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+namespace OCA\Files_Versions\Sabre;
+
+use OCP\Files\IRootFolder;
+use Sabre\DAV\Exception\Forbidden;
+use Sabre\DAV\ICollection;
+
+class VersionHome implements ICollection {
+
+	/** @var array */
+	private $principalInfo;
+
+	/** @var IRootFolder */
+	private $rootFolder;
+
+	public function __construct(array $principalInfo, IRootFolder $rootFolder) {
+		$this->principalInfo = $principalInfo;
+		$this->rootFolder = $rootFolder;
+	}
+
+	public function delete() {
+		throw new Forbidden('Permission denied to delete your trashbin');
+	}
+
+	public function getName(): string {
+		list(,$name) = \Sabre\Uri\split($this->principalInfo['uri']);
+		return $name;
+	}
+
+	public function setName($name) {
+		throw new Forbidden('Permission denied to rename this trashbin');
+	}
+
+	public function createFile($name, $data = null) {
+		throw new Forbidden('Not allowed to create files in the trashbin');
+	}
+
+	public function createDirectory($name) {
+		throw new Forbidden('Not allowed to create folders in the trashbin');
+	}
+
+	public function getChild($name) {
+		list(,$userId) = \Sabre\Uri\split($this->principalInfo['uri']);
+
+		if ($name === 'versions') {
+			return new VersionRoot($userId, $this->rootFolder);
+		}
+	}
+
+	public function getChildren() {
+		list(,$userId) = \Sabre\Uri\split($this->principalInfo['uri']);
+
+		return [
+			new VersionRoot($userId, $this->rootFolder),
+		];
+	}
+
+	public function childExists($name) {
+
+		return $name === 'versions';
+	}
+
+	public function getLastModified() {
+		return 0;
+	}
+}
